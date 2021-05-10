d3168d2b14a61b080f2f344c179ced19dc520115
Sevdet IŞIK <63150729+SevdetNeng@users.noreply.github.com>
2021-05-10 21:21:27 +0300

    Add files via upload

---
+++ Asal Carpanlar Uygulaması/NDPodev2/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace NDPodev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblY = new System.Windows.Forms.Label();
+            this.btnSon = new System.Windows.Forms.Button();
+            this.btnArk = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.lblX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.BackColor = System.Drawing.Color.LightSlateGray;
+            this.lblY.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblY.Location = new System.Drawing.Point(46, 128);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(16, 15);
+            this.lblY.TabIndex = 2;
+            this.lblY.Text = "Y";
+            this.lblY.Click += new System.EventHandler(this.lblY_Click);
+            // 
+            // btnSon
+            // 
+            this.btnSon.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnSon.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSon.Location = new System.Drawing.Point(164, 184);
+            this.btnSon.Name = "btnSon";
+            this.btnSon.Size = new System.Drawing.Size(87, 23);
+            this.btnSon.TabIndex = 5;
+            this.btnSon.Text = "SON";
+            this.btnSon.UseVisualStyleBackColor = false;
+            this.btnSon.Click += new System.EventHandler(this.btnSon_Click);
+            // 
+            // btnArk
+            // 
+            this.btnArk.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnArk.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnArk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnArk.Location = new System.Drawing.Point(49, 183);
+            this.btnArk.Name = "btnArk";
+            this.btnArk.Size = new System.Drawing.Size(83, 23);
+            this.btnArk.TabIndex = 6;
+            this.btnArk.Text = "ARKADAŞ MI";
+            this.btnArk.UseVisualStyleBackColor = false;
+            this.btnArk.Click += new System.EventHandler(this.btnArk_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtX.Location = new System.Drawing.Point(92, 39);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(159, 20);
+            this.txtX.TabIndex = 7;
+            this.txtX.TextChanged += new System.EventHandler(this.txtX_TextChanged);
+            // 
+            // txtY
+            // 
+            this.txtY.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtY.Location = new System.Drawing.Point(92, 128);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(159, 20);
+            this.txtY.TabIndex = 8;
+            this.txtY.TextChanged += new System.EventHandler(this.txtY_TextChanged);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.BackColor = System.Drawing.Color.LightSlateGray;
+            this.lblX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblX.Location = new System.Drawing.Point(46, 42);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(16, 15);
+            this.lblX.TabIndex = 9;
+            this.lblX.Text = "X";
+            this.lblX.Click += new System.EventHandler(this.lblX_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 232);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnArk);
+            this.Controls.Add(this.btnSon);
+            this.Controls.Add(this.lblY);
+            this.Name = "Form1";
+            this.Text = "Arkadaş Sayılar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Button btnSon;
+        private System.Windows.Forms.Button btnArk;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label lblX;
+    }
+}
+
---
+++ Asal Carpanlar Uygulaması/NDPodev2/Form1.cs
@@ -0,0 +1,348 @@
+﻿/********************************************************************************************************************************************************
+**                                                                                                                                                     **
+**                                                                                                                                                     **
+**                                                                          SAKARYA ÜNİVERSİTESİ                                                       **                                                      
+**                                                                BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ                                            ** 
+**                                                                      BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ                                                 ** 
+**                                                                     NESNEYE DAYALI PROGRAMLAMA DERSİ                                                **
+**                                                                          2019-2020 BAHAR DÖNEMİ                                                     **
+**                                                                                                                                                     ** 
+**                                                                ÖDEV NUMARASI...........:2                                                           ** 
+**                                                                ÖĞRENCİ ADI.............:SEVDET IŞIK                                                 **                 
+**                                                                ÖĞRENCİ NUMARASI........:G191210082                                                  **
+**                                                                DERSİN ALINDIĞI GRUP....:2.ÖĞRETİM B GRUBU                                           **
+**                                                                                                                                                     **
+**                                                                                                                                                     **   
+**                                                                                                                                                     **
+********************************************************************************************************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NDPodev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        //Textbox'a girilen girdinin sayi olup olmadığını kontrol edecek olan method.
+        static bool digitControl(string giris)
+        {
+            bool donus = true;
+            //Girilen girdinin her bir karakteri tek tek kontrol ediliyor.
+            for(int i = 0; i < giris.Length; i++)
+            {
+                //Eğer girilen girdinin ilgili karakteri sayı değilse donus değişkeni false olarak atanıyor.
+                if (!char.IsDigit(giris[i]))
+                {
+                    donus = false;
+                }
+            }
+            return donus;
+        }
+       
+                   
+        private void lblY_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        //Son butonuna tıklanınca form uygulaması kapatılıyor.
+        private void btnSon_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public int btnSayac = 0;
+
+        //Arkadas mı butonunun yaptığı kontroller.
+        private void btnArk_Click(object sender, EventArgs e)
+        { 
+            //Eğer methodun dışında tanımlanan btnSayac değeri 0'sa kontrole giriliyor.      
+            if (btnSayac == 0)
+            {   
+                //Sayı değeri girilene kadar kullanıcıdan girdi alınıyor.
+                while (true)
+                {
+                    int x;
+                    int y;
+                    //Eğer girilen girdiler sayıysa ilgili değişkenlere atanıyor.
+                    if (digitControl(txtX.Text) && digitControl(txtY.Text))
+                    {
+                         x = Convert.ToInt32(txtX.Text);
+                         y = Convert.ToInt32(txtY.Text);
+                    }
+                    //Eğer girilen girdilerden herhangi biri sayı değilse tekrar girilmesine dair mesaj gösteriliyor.
+                    else
+                    {
+                        MessageBox.Show("Lütfen boşluğa sayı giriniz.");
+                        break;
+                    }
+                    
+                    
+                    int xToplam = 0;
+                    int yToplam = 0;
+                    int[] xCarpan = new int[100];
+                    int[] yCarpan = new int[100];
+                    int xSayac = 0;
+                    int ySayac = 0;
+
+                    //Girilen değerlerden x'in çarpanları bulunuyor.
+                    for (int i = 1; i < x; i++)
+                    {
+                        //eğer sayı x'in çarpanıysa ilgili diziye eklenip,toplam değere ekleniyor.
+                        if (x % i == 0)
+                        {
+                            xCarpan[xSayac] = i;
+                            xSayac++;
+                            xToplam += i;
+
+                        }
+                    }
+
+                    //Girilen değerlerden y'nin çarpanları bulunuyor.
+                    for (int i = 1; i < y; i++)
+                    {
+                        //eğer sayı y'nin çarpanıysa ilgili diziye eklenip,toplam değere ekleniyor.
+                        if (y % i == 0)
+                        {
+                            yCarpan[ySayac] = i;
+                            ySayac++;
+                            yToplam += i;
+                        }
+                    }
+                    this.Size = new Size(800, 450);
+
+                    Label lblYeniX = new Label();
+                    lblYeniX.BackColor = System.Drawing.Color.LightSlateGray;
+                    lblYeniX.Size = new Size(14, 13);
+                    lblYeniX.Location = new Point(400, 30);
+                    lblYeniX.Text = "X";
+                    Controls.Add(lblYeniX);
+
+                    ListBox lbX = new ListBox();
+                    lbX.Size = new Size(100, 300);
+
+                    lbX.Location = new Point(355, 50);
+
+                    //x'in çarpanları ilgili listboxa değer olarak ekleniyor.
+                    for (int i = 0; i < xSayac; i++)
+                    {
+                        lbX.Items.Add(xCarpan[i]);
+                    }
+
+                    Controls.Add(lbX);
+
+                    Label lblYeniY = new Label();
+                    lblYeniY.Size = new Size(14, 13);
+                    lblYeniY.BackColor = System.Drawing.Color.LightSlateGray;
+                    lblYeniY.Location = new Point(600, 30);
+                    lblYeniY.Text = "Y";
+                    Controls.Add(lblYeniY);
+
+                    ListBox lbY = new ListBox();
+                    lbY.Size = new Size(100, 300);
+                    lbY.Location = new Point(555, 50);
+                    //y'nin çarpanları ilgili listboxa değer olarak ekleniyor.
+                    for (int i = 0; i < ySayac; i++)
+                    {
+                        lbY.Items.Add(yCarpan[i]);
+                    }
+                    Controls.Add(lbY);
+
+                    Label lblYeniToplam = new Label();
+                    lblYeniToplam.Text = "TOPLAMLAR";
+                    lblYeniToplam.Location = new Point(250, 350);
+                    lblYeniToplam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    lblYeniToplam.Size = new Size(80, 15);
+
+                    lblYeniToplam.BackColor = System.Drawing.Color.LightSlateGray;
+                    Controls.Add(lblYeniToplam);
+
+                    TextBox txtXToplam = new TextBox();
+                    TextBox txtYToplam = new TextBox();
+
+                    txtXToplam.Location = new Point(355, 347);
+                    txtXToplam.Text = Convert.ToString(xToplam);
+                    txtXToplam.BackColor = System.Drawing.Color.LightSlateGray;
+                    txtXToplam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    txtXToplam.TextAlign = HorizontalAlignment.Center;
+
+                    Controls.Add(txtXToplam);
+
+                    txtYToplam.Location = new Point(555, 347);
+                    txtYToplam.Text = Convert.ToString(yToplam);
+                    txtYToplam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    txtYToplam.BackColor = System.Drawing.Color.LightSlateGray;
+                    txtYToplam.TextAlign = HorizontalAlignment.Center;
+                    Controls.Add(txtYToplam);
+                    break;
+                }                                                                                        
+            }      
+            
+            //Eğer btnSayac değeri 0'dan farklıysa aşağıdaki işlemler uygulanıyor.
+            else
+            {   
+                //Kullanıcı sayı girene kadar girdi girilmesi isteniyor.
+                while (true)
+                {
+                    //Uygulamanın birden fazla kez çalıştırılabilir olması için form ekranı temizleniyor ve kontrol elemanları tekrar ekleniyor.
+                    this.Controls.Clear();
+                    this.Controls.Add(txtX);
+                    this.Controls.Add(txtY);
+                    this.Controls.Add(lblX);
+                    this.Controls.Add(lblY);
+                    this.Controls.Add(btnArk);
+                    this.Controls.Add(btnSon);
+                    int x;
+                    int y;
+                    //Eğer girilen girdiler sayıysa ilgili değişkenlere atanıyor.
+                    if (digitControl(txtX.Text) && digitControl(txtY.Text))
+                    {
+                        x = Convert.ToInt32(txtX.Text);
+                        y = Convert.ToInt32(txtY.Text);
+                    }
+                    //Eğer girdilerden herhangi biri sayı değilse tekrar girilmesine dair mesaj gösteriliyor.
+                    else
+                    {
+                        MessageBox.Show("Lütfen boşluğa sayı giriniz.");
+                        break;
+                    }
+
+
+                    int xToplam = 0;
+                    int yToplam = 0;
+                    int[] xCarpan = new int[100];
+                    int[] yCarpan = new int[100];
+                    int xSayac = 0;
+                    int ySayac = 0;
+
+                    //Tekrar girilen x değerinin çarpanları bulunuyor.
+                    for (int i = 1; i < x; i++)
+                    {
+                        //Eğer sayı x'in çarpanıysa diziye eklenip,toplam değere ekleniyor.
+                        if (x % i == 0)
+                        {
+                            xCarpan[xSayac] = i;
+                            xSayac++;
+                            xToplam += i;
+
+                        }
+                    }
+
+                    //Tekrar girilen y değerinin çarpanları bulunuyor.
+                    for (int i = 1; i < y; i++)
+                    {
+                        //Eğer sayı y'nin çarpanıysa diziye eklenip,toplam değere ekleniyor.
+                        if (y % i == 0)
+                        {
+                            yCarpan[ySayac] = i;
+                            ySayac++;
+                            yToplam += i;
+                        }
+                    }
+                    this.Size = new Size(800, 450);
+
+                    Label lblYeniX = new Label();
+                    lblYeniX.BackColor = System.Drawing.Color.LightSlateGray;
+                    lblYeniX.Size = new Size(14, 13);
+                    lblYeniX.Location = new Point(400, 30);
+                    lblYeniX.Text = "X";
+                    Controls.Add(lblYeniX);
+
+                    ListBox lbX = new ListBox();
+                    lbX.Size = new Size(100, 300);
+
+                    lbX.Location = new Point(355, 50);
+                    //Yeni girilen x'in çarpanları ilgili listboxa değer olarak ekleniyor.
+                    for (int i = 0; i < xSayac; i++)
+                    {
+                        lbX.Items.Add(xCarpan[i]);
+                    }
+
+                    Controls.Add(lbX);
+
+                    Label lblYeniY = new Label();
+                    lblYeniY.Size = new Size(14, 13);
+                    lblYeniY.BackColor = System.Drawing.Color.LightSlateGray;
+                    lblYeniY.Location = new Point(600, 30);
+                    lblYeniY.Text = "Y";
+                    Controls.Add(lblYeniY);
+
+                    ListBox lbY = new ListBox();
+                    lbY.Size = new Size(100, 300);
+                    lbY.Location = new Point(555, 50);
+                    //Yeni girilen y'nin çarpanları ilgili listboxa değer olarak ekleniyor.
+                    for (int i = 0; i < ySayac; i++)
+                    {
+                        lbY.Items.Add(yCarpan[i]);
+                    }
+                    Controls.Add(lbY);
+
+                    Label lblYeniToplam = new Label();
+                    lblYeniToplam.Text = "TOPLAMLAR";
+                    lblYeniToplam.Location = new Point(250, 350);
+                    lblYeniToplam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    lblYeniToplam.Size = new Size(80, 15);
+
+                    lblYeniToplam.BackColor = System.Drawing.Color.LightSlateGray;
+                    Controls.Add(lblYeniToplam);
+
+                    TextBox txtXToplam = new TextBox();
+                    TextBox txtYToplam = new TextBox();
+
+                    txtXToplam.Location = new Point(355, 347);
+                    txtXToplam.Text = Convert.ToString(xToplam);
+                    txtXToplam.BackColor = System.Drawing.Color.LightSlateGray;
+                    txtXToplam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    txtXToplam.TextAlign = HorizontalAlignment.Center;
+
+                    Controls.Add(txtXToplam);
+
+                    txtYToplam.Location = new Point(555, 347);
+                    txtYToplam.Text = Convert.ToString(yToplam);
+                    txtYToplam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    txtYToplam.BackColor = System.Drawing.Color.LightSlateGray;
+                    txtYToplam.TextAlign = HorizontalAlignment.Center;
+                    Controls.Add(txtYToplam);
+                    break;
+                }
+
+            }
+
+            btnSayac++;                   
+        }
+
+        private void txtX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtY_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblX_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
